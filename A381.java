24e152929bb7dd781fc02d74cb47014650653b0e
Roshan <1roshanekka@gmail.com>
2023-08-18 18:18:14 +0530

    A381

--- A381.java
+++ A381.java
@@ -9,7 +9,7 @@ public class A381 {
 		
 		// as the cards are distinct we can sort them and the players can pick alternatively
 		
-		int arr[] = new int[n]; // creating array
+		// int arr[] = new int[n]; // creating array
 		
 		Deque<Integer> deque = new LinkedList<>();
 
@@ -20,53 +20,11 @@ public class A381 {
 			//int num = sc.nextInt();
 			//arr[i] = num;
 		}
-		
-		// selection sort
-		
-		/*		
-		// descending order
-		for ( int i = 0 ; i < n ; i++) {
-			for ( int j = 0 ; j < n-1 ; j++) {
-				if( arr[j] < arr[j+1] ) {
-					int dummy = arr[j+1];
-					arr[j+1] = arr [j];
-					arr[j] = dummy;
-				}
-			}
-		}
-		*/
 
 		// Sereja picks first then Dima and this order continues
-                int serejaScore = 0 ;
-                int dimaScore = 0;
+		int serejaScore = 0 ;
+		int dimaScore = 0;
 		
-		/*
-		int count = 0;
-		while(arr.length()>0) {
-			int add = 0;
-			if ( count%2==0 ){
-				if( arr.slice(0) > arr.slice(-1) ) {
-					add = arr[0];
-					arr.remove(arr[0]);
-				}
-				else {
-					add = arr[1];
-					arr.remove(arr[1]);
-				}
-				serejaScore = serejaScore + add;
-			}
-			else {
-				if( arr.slice(0) > arr.slice(-1) ) {
-					add = arr[0];
-				}
-				else {
-					add = arr[1];
-				}
-				dimaScore = dimaScore + add;
-			}
-			count++ ; 
-		}
-		*/
 		
 		int count = 0 ;
 		while ( deque.size() > 0 ) {
@@ -99,27 +57,6 @@ public class A381 {
 			}
 			count++;
 		}
-		// get sorted array
-		
-		// for( int i = 0 ; i < n ; i++ ) {
-		// 	System.out.print(arr[i] + " ");
-		// }
-		
-		
-		// Sereja picks first then Dima and this order continues
-		/*
-		int serejaScore = 0 ;
-		int dimaScore = 0;
-
-		for( int i = 0 ; i < n ; i++ ) {
-			if(i%2==0) {
-				serejaScore = serejaScore + arr[i];
-			}	
-			else {
-				dimaScore = dimaScore + arr[i];
-			}
-		}
-		*/
 	
 		// printing score
 
