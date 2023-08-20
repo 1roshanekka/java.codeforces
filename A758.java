4d75d3926c0ba92ec5ca15d93c32399232c7bca7
Roshan <1roshanekka@gmail.com>
2023-08-20 11:47:32 +0530

    A758

--- A758.java
+++ A758.java
@@ -21,9 +21,12 @@ public class A758 {
 			max = arr[0];
 		}
 		else {
-			for ( int i = 0 ; i < n-1 ; i++ ) {
-				if( arr[i] < arr[i+1] ) {
-					max = arr[i+1];
+			for ( int i = 0 ; i < n ; i++ ) {
+				// if( arr[i] < arr[i+1] ) {
+				// 	max = arr[i+1];
+				// }
+				if ( max < arr[i]) {
+					max = arr[i];
 				}
 			}
 		}
